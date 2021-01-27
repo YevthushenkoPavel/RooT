19c2b0af1413bbc35365691d831c2efd30ae0157
Val1gun <vadymvaligun@gmail.com>
2021-01-27 13:23:24 +0200

    third step

--- main_gusto/views.py
+++ main_gusto/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Dish
 
 
 # Create your views here.
 def main(request):
-    return render(request, 'index.html')
+    categories = Category.objects.filter(is_visible=True).order_by('category_order')
+    for item in categories:
+        item.dishes = Dish.objects.filter(category=item.pk)
+    # categories = Category.objects.all()
+    return render(request, 'index.html', context={'categories': categories})
